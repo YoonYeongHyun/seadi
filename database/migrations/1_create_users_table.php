f08a14ae4b088e6b45d6da87f6a3d543b92fa966
choyoungrae <choyoungrae-flx>
2024-09-25 23:09:22 +0900

    마이그레이션

--- database/migrations/1_create_users_table.php
+++ database/migrations/1_create_users_table.php
@@ -12,15 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->id(); // 사용자 관리번호 (no)
+            $table->string('username')->unique(); // 사용자명
+            $table->string('email')->unique(); // 이메일
+            $table->string('password'); // 비밀번호
+            $table->tinyInteger('level')->default(1); // 사용자 레벨 (1~99)
+            $table->boolean('subscription_required')->default(false); // 유료 구독 여부
+            $table->integer('total_responses')->default(0); // 총 답변 수
+            $table->integer('total_request')->default(0); // 총 요청 수
+            $table->timestamp('email_verified_at')->nullable(); // 이메일 인증
+            $table->string('two_factor_secret')->nullable(); // 2단계 인증
+            $table->string('two_factor_recovery_codes')->nullable(); // 2단계 인증 복구 코드
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
-            $table->timestamps();
+            $table->timestamps(); // 가입일, 수정일 (dt_reg, dt_upt)
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ database/migrations/2024_09_25_134439_create_requests_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id(); // 요청 고유 식별자 (no)
+            $table->foreignId('no_user')->constrained('users')->onDelete('cascade'); // 요청자 사용자 ID (no_user)
+            $table->string('title'); // 아이디어 제목
+            $table->text('content'); // 아이디어 설명
+            $table->boolean('is_paid')->default(false); // 유료/무료 여부
+            $table->timestamps(); // 등록일, 수정일 (created_at, updated_at)
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+
+};
