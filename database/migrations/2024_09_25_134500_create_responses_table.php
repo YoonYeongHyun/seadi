cce7e4556f685ce1c9bcf7fe52764172835dae3f
choyoungrae <choyoungrae-flx>
2024-09-25 23:58:30 +0900

    마이그레이션

--- database/migrations/2024_09_25_134500_create_responses_table.php
+++ database/migrations/2024_09_25_134500_create_responses_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('responses', function (Blueprint $table) {
-            $table->id(); // 답변 고유 식별자 (no)
+            $table->id(); // 답변 고유 식별자 (no)2
             $table->foreignId('no_request')->constrained('requests')->onDelete('cascade'); // 아이디어 요청 ID (no_request)
             $table->foreignId('no_user')->constrained('users')->onDelete('cascade'); // 답변자 사용자 ID (no_user)
             $table->text('content'); // 답변 내용
