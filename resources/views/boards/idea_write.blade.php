44d01c62a6d1c804b39a9ca33715732b7f1f48e5
YoonYeongHyun <yyh455@naver.com>
2024-10-01 22:19:23 +0900

    quill 추가 summernote주석처리

--- resources/views/boards/idea_write.blade.php
+++ resources/views/boards/idea_write.blade.php
@@ -13,13 +13,18 @@
     <div class="container mx-auto max-w-7xl pt-12 pb-24">
         <div class="">
             <form method="post">
-                <textarea id="summernote" name="editordata"></textarea>
+                <div id="quill-editor" class="mb-3" style="height: 300px;">
+                </div> 
+                <textarea rows="3" class="mb-3 hidden" name="job_description" id="quill-editor-area">
+                </textarea> 
+                <!--<textarea id="summernote" name="editordata"></textarea>-->
             </form>
         </div>
     </div>
 </x-app-layout>
 
 <script>
+    /*
     $(document).ready(function() {
         //여기 아래 부분
         $('#summernote').summernote({
@@ -32,10 +37,31 @@
             
         });
     });
+    */    
+
+    document.addEventListener('DOMContentLoaded', function() { 
+        if (document.getElementById('quill-editor-area')) { 
+            var editor = new Quill('#quill-editor', { 
+                theme: 'snow' 
+            }); 
+            var quillEditor = document.getElementById('quill-editor-area'); 
+            editor.on('text-change', function() { 
+                quillEditor.value = editor.root.innerHTML; 
+            }); 
+            quillEditor.addEventListener('input', function() { 
+                editor.root.innerHTML = quillEditor.value; 
+            }); 
+        } 
+    });
 </script>
+<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet"> 
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+<!--
 <script src="{{ asset('js/summernote') }}/summernote-lite.js"></script>
 <script src="{{ asset('js/summernote') }}/lang/summernote-ko-KR.js"></script>
 <link rel="stylesheet" href="{{ asset('css/summernote') }}/summernote-lite.css">
+-->
+
 <?php
 function Console_log($data){
     echo "<script>console.log( 'PHP_Console: " . $data . "' );</script>";
